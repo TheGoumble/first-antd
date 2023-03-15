d75bb9dc5c9dfecc5347c7df48b395afb3ae6225
Javier Vargas <3231javiervargas@gmail.com>
2023-03-14 21:15:10 -0400

    creating a AntD form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import "antd/dist/antd.css"
-import Login from "./components/Login"
+import "./App.css"
+import AntdForm from "./components/AntdForm"
 
 function App() {
   return (
-    <>
-      <h1>My first ant design</h1>
-      <Login />
-    </>
+    <section id="App">
+      <div id="App-content ">
+        <h1>My first ant design</h1>
+        <AntdForm />
+      </div>
+    </section>
   )
 }
 
